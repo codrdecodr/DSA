57b30c6513f04af91d11eeb7bff48cf940a00c3f
HappyLoops <165475864+codrdecodr@users.noreply.github.com>
2026-07-04 20:28:10 +0530

    Time: 69 ms (17.28%), Space: 151 MB (48.62%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st; //element, min uptil that point
+    MinStack() {}
+    void push(int value) {
+        if(st.empty()){
+            st.push({value,value});
+        }
+        else{
+            st.push({value,min(value,getMin())});
+        }
+    }
+    void pop() {
+        st.pop();
+    }
+    int top() {
+        return st.top().first;
+    }
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
