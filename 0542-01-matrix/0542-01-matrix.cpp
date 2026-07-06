4f3fd90422261f716b4703ea2d9a1d752ef6c494
HappyLoops <165475864+codrdecodr@users.noreply.github.com>
2026-07-07 01:36:21 +0530

    Time: 15 ms (72.29%), Space: 34.4 MB (96.89%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+        queue<pair<int,int>> q;
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n ;j++){
+                if(mat[i][j] == 0){
+                    q.push({i,j});
+                }
+                else{
+                    mat[i][j] = INT_MAX;
+                }
+            }
+        }
+        vector<int> delRow = {-1,0,1,0};
+        vector<int> delCol = {0,1,0,-1};
+        while(!q.empty()){
+            int k = q.size();
+            while(k--){
+                auto it = q.front();
+                q.pop();
+                int row = it.first;
+                int col = it.second;
+
+                for(int i = 0; i < 4 ; i++){
+                    int nrow = row + delRow[i];
+                    int ncol = col + delCol[i];
+
+                    if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && mat[nrow][ncol] > mat[row][col] + 1){
+                        mat[nrow][ncol] = mat[row][col] + 1;
+                        q.push({nrow,ncol});
+                    }
+                }
+
+            }
+        }
+        return mat;
+    }
+};
