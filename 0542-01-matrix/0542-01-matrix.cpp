b4972e5c49d91b2602632af2b4cee0906c026c1b
HappyLoops <165475864+codrdecodr@users.noreply.github.com>
2026-07-12 22:54:19 +0530

    Time: 22 ms (43.39%), Space: 34.7 MB (77.62%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,7 +5,7 @@ public:
         int n = mat[0].size();
         queue<pair<int,int>> q;
         for(int i = 0; i < m ; i++){
-            for(int j = 0; j < n ;j++){
+            for(int j = 0; j < n; j++){
                 if(mat[i][j] == 0){
                     q.push({i,j});
                 }
@@ -18,22 +18,20 @@ public:
         vector<int> delCol = {0,1,0,-1};
         while(!q.empty()){
             int k = q.size();
-            while(k--){
+            for(int i = 0 ; i < k ; i++){
                 auto it = q.front();
                 q.pop();
                 int row = it.first;
                 int col = it.second;
-
                 for(int i = 0; i < 4 ; i++){
                     int nrow = row + delRow[i];
                     int ncol = col + delCol[i];
 
-                    if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && mat[nrow][ncol] > mat[row][col] + 1){
+                    if(nrow>=0 && nrow < m && ncol >= 0 && ncol < n && mat[nrow][ncol] > mat[row][col] + 1){
                         mat[nrow][ncol] = mat[row][col] + 1;
                         q.push({nrow,ncol});
                     }
                 }
-
             }
         }
         return mat;
