2293ddfa06d3b0cbe83926bbd5f18e7bfaf8ca06
HappyLoops <165475864+codrdecodr@users.noreply.github.com>
2026-09-20 13:22:11 +0530

    Time: 55 ms (7.61%), Space: 31.1 MB (5.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    void bfs(int i, int j, vector<vector<bool>>& vis,
-             vector<vector<char>>& grid) {
-        vis[i][j] = true;
-        queue<pair<int, int>> q;
-        q.push({i, j});
+    bool isvalid(int i,int j,int m,int n){
+        if(i < 0 || i >= m) return false;
+        if(j < 0 || j >= n) return false;
+        return true;
+    }
+    void bfs(int i,int j,vector<vector<char>>& grid,vector<vector<int>>& vis){
         int m = grid.size();
         int n = grid[0].size();
-        while (!q.empty()) {
-            auto it = q.front();
+        vis[i][j] = 1;
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            int row = it.first;
-            int col = it.second;
-            vector<int> delRow = {-1, 0, 1, 0};
-            vector<int> delCol = {0, 1, 0, -1};
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-
-                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
-                    grid[nrow][ncol] == '1' && vis[nrow][ncol] == false) {
-                    vis[nrow][ncol] = true;
-                    q.push({nrow, ncol});
+            vector<int> dr = {-1,0,1,0};
+            vector<int> dc = {0,-1,0,1};
+            for(int i = 0; i < 4 ; i++){
+                int nrow = row + dr[i];
+                int ncol = col + dc[i];
+                if(isvalid(nrow,ncol,m,n) && grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
                 }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
+        int islands = 0;
         int m = grid.size();
         int n = grid[0].size();
-        int cnt = 0;
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && vis[i][j] == false) {
-                    cnt++;
-                    bfs(i, j, vis, grid);
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        for(int i = 0; i < m ; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1' && vis[i][j] == 0){
+                    islands++;
+                    bfs(i,j,grid,vis);
                 }
             }
         }
-        return cnt;
+        return islands;
     }
 };
