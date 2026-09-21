e383d838e382b98ef36d95a27301e763daa5aa47
HappyLoops <165475864+codrdecodr@users.noreply.github.com>
2026-09-21 20:59:36 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans;
-        for (int i = 0; i < n; i++) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
-                ans.push_back(nums[i]);
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
-        while (ans.size() < n) {
-            ans.push_back(0);
-        }
-        nums = ans;
     }
 };
